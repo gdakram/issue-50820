2dffe1e68a36b325604b7e06e9b6082d73e4f96e
Shahrier Akram <shahrier.akram@gmail.com>
2025-08-11 20:59:03 -0700

    Make charles inspectable and make network calls.

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { NativeModules, Platform, StyleSheet, Text, View } from 'react-native';
 
 function createBlockingDelay(milliseconds: number) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const startTime = Date.now();
     while (Date.now() - startTime < milliseconds) {
       // Busy-waiting loop to simulate synchronous delay
     }
-    resolve(true);
+    fetch('https://www.nfl.com/?headless-promise').then(() => resolve(true));
   });
 }
 
@@ -15,8 +15,18 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    createBlockingDelay(1000).then(() => {
-      setCounter((previousCount) => previousCount + 1);
+    const intervalId = setInterval(() => {
+      fetch('https://www.nfl.com/?headless-interval').catch(error => {
+        console.error('Fetch error:', error);
+      });
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    createBlockingDelay(2000).then(() => {
+      setCounter(previousCount => previousCount + 1);
     });
   }, [counter]);
 
